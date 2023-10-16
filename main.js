dfbd5fd3873af6e7830805c4b886641a8d239e9b
Cocera <alcocera@gmail.com>
2023-10-16 13:32:46 +0200

    get arr questions

--- main.js
+++ main.js
@@ -6,6 +6,49 @@ const homeBody = document.getElementById('home-body');
 const questionBody = document.getElementById('question-body');
 const resultsBody = document.getElementById('results-body');
 
+const questionTxt = document.getElementById('question');
+
+// ---------- Set SPA
+
+const hideAll = () => {
+    homeBody.classList.add('hide');
+    questionBody.classList.add('hide');
+    resultsBody.classList.add('hide');
+};
+
+const showHomeBody = () => {
+    hideAll();
+    homeBody.classList.remove('hide');
+};
+
+const showQuestionBody = () => {
+    hideAll();
+    questionBody.classList.remove('hide');
+};
+
+const showResultsBody = () => {
+    hideAll();
+    resultsBody.classList.remove('hide');
+};
+
+homeNav.addEventListener('click', showHomeBody);
+questionNav.addEventListener('click', showQuestionBody);
+resultsNav.addEventListener('click', showResultsBody);
+
+// ---------- Get API questions
+
+let questionsAll = [];
+
 axios.get('https://opentdb.com/api.php?amount=10&category=20')
-    .then(question => console.log(question.results))
+    .then(question => {
+            questionsAll = question.data.results;
+            console.log(questionsAll);
+            // question.data.results.forEach(object => {
+            //     questionsAll.push(object.question);
+            //     answersAll.push(object.correct_answer, object.incorrect_answers)
+            // })
+    })
     .catch(err => console.error(err,'ERROR'));
+
+
+
