9b30278db39edf511ab89a62a75243996e07b174
Cocera <alcocera@gmail.com>
2023-10-16 12:10:19 +0200

    basic DOM

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-console.log('OK JS')
+const homeNav = document.getElementById('home-nav');
+const questionNav = document.getElementById('question-nav');
+const resultsNav = document.getElementById('results-nav');
+
+const homeBody = document.getElementById('home-body');
+const questionBody = document.getElementById('question-body');
+const resultsBody = document.getElementById('results-body');
 
 axios.get('https://opentdb.com/api.php?amount=10&category=20')
-.then(question => console.log(question))
-.catch(err => console.error(err,'ERROR'))
+    .then(question => console.log(question.results))
+    .catch(err => console.error(err,'ERROR'));
